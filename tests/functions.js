fb60ac16abbb3a821ab807dad4ea024a947c9bc1
Tommy Vercety <donvercety@gmail.com>
2015-05-01 20:03:06 +0300

    form selectors: input, textarea and select - added

--- tests/functions.js
+++ tests/functions.js
@@ -1,34 +1,3 @@
-var form = document.getElementById('my-form'); 
-form.addEventListener("submit", function(evt){
-    evt.preventDefault();
-    var formData = getFormData('my-form');
-
-    v.validate(formData.user).required().minLength(8).isAlphaDash().noWhitespace();
-    console.log('Value: ' + formData.user, 'Verdict: ' + v.isValid());
-    console.log('Errors: ' + v.getErrors());
-
-    v.validate(formData.pass).required().minLength(12);
-    console.log('Value: ' + formData.pass, 'Verdict: ' + v.isValid());
-    console.log('Errors: ' + v.getErrors());
-});
-
-function getFormData(formID) {
-    var container, inputs, len, index, data = {};
-
-    // get the container element
-    container = document.getElementById(formID);
-
-    // find it's child 'input' elements
-    inputs = container.getElementsByTagName('input');
-    for(index = 0, len = inputs.length; index < len;  index++) {
-        if(inputs[index].name) {
-            data[inputs[index].name] = inputs[index].value;
-        }    
-    }
-
-    return data;
-}
-
 function assert(value, desc) {
 	var resultsList = document.getElementById("results");
 	if (!resultsList) {
--- validate.js
+++ validate.js
@@ -76,6 +76,47 @@ MY.Validate = (function() {
     // :: Public Methods
     // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
     
+    Validate.prototype.getFormFields = function(formID) {
+        var container, inputs, index, len, data = {};
+
+        // get the container element
+        container = document.getElementById(formID);
+
+        // find it's child 'input' elements
+        inputs = container.getElementsByTagName('input');
+        for(index = 0, len = inputs.length; index < len;  index++) {
+            if(inputs[index].name) {
+                data[inputs[index].name] = inputs[index].value;
+            }    
+        }
+
+        // find it's child 'textarea' elements
+        var textarea, t_index, tl;
+        textarea = container.getElementsByTagName('textarea');
+        for(t_index = 0, tl = textarea.length; t_index < tl;  t_index++) {
+            if(textarea[t_index].name) {
+                data[textarea[t_index].name] = textarea[t_index].value;
+            }    
+        }
+
+        // find it's child 'select' elements
+        var select, option, s_index, sl;
+        select = container.getElementsByTagName('select');
+        for(s_index = 0, sl = select.length; s_index < sl;  s_index++) {
+            if(select[s_index].name) {
+                option = select[s_index].getElementsByTagName('option');
+                var o_index, ol;
+                for(o_index = 0, ol = option.length; o_index < ol;  o_index++) {
+                    if(option[o_index].selected) {
+                        data[select[s_index].name] = option[o_index].value;
+                    }
+                }
+            }    
+        }
+
+        return data;
+    };
+    
     /**
      * Supply the value to be validated
      * @param  {[mixed]} value
@@ -96,10 +137,10 @@ MY.Validate = (function() {
 
     /**
      * Get errors, if any, after running isValid()
-     * @return {[type]} [description]
+     * @return {[mixed]} [returns errors array, if no errors, returns false]
      */
     Validate.prototype.getErrors = function() {
-        return lastErrors;
+        return lastErrors.length > 0 ? lastErrors : false;
     };
 
     /**
